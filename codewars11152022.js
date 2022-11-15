4c607dd4ba63fe475cd6b8e77daa59ddd9924462
Willim <tundra2727@gmail.com>
2022-11-15 11:01:21 -0800

    daily codewars added

---
+++ codewars11152022.js
@@ -0,0 +1,20 @@
+// Title: How many light sabers do you own.
+
+// Instructions
+// Output
+// Inspired by the development team at Vooza, write the function that
+
+// accepts the name of a programmer, and
+// returns the number of lightsabers owned by that person.
+// The only person who owns lightsabers is Zach, by the way. He owns 18, which is an awesome number of lightsabers. Anyone else owns 0.
+
+// Note: your function should have a default parameter.
+
+// For example(Input --> Output):
+
+// "anyone else" --> 0
+// "Zach" --> 18
+
+// my solution =>
+
+howManyLightsabersDoYouOwn = (name) => name === 'Zach' ? 18 : 0;
