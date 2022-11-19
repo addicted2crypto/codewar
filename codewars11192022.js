3b6e887a08e7a6f0a57ce6864f007cf4811810f4
Willim <tundra2727@gmail.com>
2022-11-19 12:57:13 -0800

    daily codewars added 11/19/2022

---
+++ codewars11192022.js
@@ -0,0 +1,14 @@
+// Title: Squaring a argument. 
+
+// DESCRIPTION:
+// Now you have to write a function that takes an argument and returns the square of it.
+
+// FUNDAMENTALS
+
+// My solution =>
+
+square = (x) => x**2;
+
+// function square(x){
+//   return x ** 2;
+// }
