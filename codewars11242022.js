29379f185edade6aa1672d79876a5960ed4df340
Willim <tundra2727@gmail.com>
2022-11-24 14:11:08 -0800

    11/24/2022 daily codewars added

---
+++ codewars11242022.js
@@ -0,0 +1,13 @@
+// Title: Multiply 
+
+// Instructions
+// Output
+// This code does not execute properly. Try to figure out why
+
+// My solution=>
+
+// function multiply(a, b){
+//  return a * b
+// }
+
+multiply = (a, b) => a * b
