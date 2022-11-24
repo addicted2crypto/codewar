fe28024de7741728e6203c4f8ef4ea82f6b5fee8
Willim <tundra2727@gmail.com>
2022-11-23 18:45:59 -0800

    daily codewars 11/23/2022

---
+++ codewars11232022.js
@@ -0,0 +1,9 @@
+// Title: Return to Sanity.
+
+// Instructions
+// Output
+// This function should return an object, but it's not doing what's intended. What's wrong?
+
+// My soution =>
+
+const mystery = () => ({sanity: 'Hello'})
