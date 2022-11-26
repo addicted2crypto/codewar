7d2ae98876f3660ba58afcd8ea87646ec501a18c
Willim <tundra2727@gmail.com>
2022-11-25 17:40:01 -0800

    daily codewars added 11/25/2022

---
+++ codewars11252022.js
@@ -0,0 +1,17 @@
+// Title: Get the mean of an array.
+
+// Instructions
+// Output
+// It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+
+// Return the average of the given array rounded down to its nearest integer.
+
+// The array will never be empty.
+
+
+// My solution =>
+
+getAverage = marks => Math.floor(marks.reduce((p,c)=> p + c) / marks.length)
+
+
+
