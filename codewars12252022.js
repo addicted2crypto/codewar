c352e19a7c9e23ebafec5e00adf12a9ae0bf41f3
Willim <tundra2727@gmail.com>
2022-12-25 10:48:07 -0800

    daily codewars added on Christmas day 2022

---
+++ codewars12252022.js
@@ -0,0 +1,15 @@
+// Descriptoion: Twice as old.
+
+// Your function takes two arguments:
+
+// current father's age (years)
+// current age of his son (years)
+// Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old). The answer is always greater or equal to 0, no matter if it was in the past or it is in the future.
+
+// FUNDAMENTALSMATHEMATICS
+
+//  My solution =>
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+    return Math.abs(2 * sonYearsOld - dadYearsOld);
+  }
