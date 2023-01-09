12ce95e36460f58f0b43cb398b2a6892fc9a43e1
Willim <tundra2727@gmail.com>
2023-01-09 08:37:25 -0800

    daily codewars added 1/9/2023

---
+++ codewars192023.js
@@ -0,0 +1,15 @@
+// Description: 5 without numbers.
+
+// Instructions
+// Output
+// Write a function that always returns 5
+
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+// Good luck :)
+
+// My solution =>
+
+function unusualFive() {
+    return ['a','b','c','d','e'].length;
+  }
