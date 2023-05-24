f16077e6ad4b2b23953dced5ba502cce19a27fb3
addicted2crypto <addicted2crypto.com>
2023-05-23 19:40:37 -0700

    daily code challenge added on 05/23/2023

---
+++ codewars05232023.js
@@ -0,0 +1,19 @@
+// Desctiption: Formatting decimal places # 0 
+
+// Instructions
+// Output
+// Each number should be formatted that it is rounded to two decimal places. You don't need to check whether the input is a valid number because only valid numbers are used in the tests.
+
+// Example:    
+// 5.5589 is rounded 5.56   
+// 3.3424 is rounded 3.34
+// FUNDAMENTALS
+
+// My Solution =>
+
+function twoDecimalPlaces(n) {
+    return Number(parseFloat(n).toFixed(2));                                                                                                                                                                                                                                             
+}
+
+refactored =>
+twoDecimalPlaces = (n) => Number(parseFloat(n).toFixed(2));
