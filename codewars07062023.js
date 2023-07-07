0fe98968b8c02681a3657b0d2932f3d38b0201b4
William <101371762+addicted2crypto@users.noreply.github.com>
2023-07-06 19:13:59 -0700

    daily code challenge for 07/06/2023
    created cloud framework/template with p5js

---
+++ codewars07062023.js
@@ -0,0 +1,27 @@
+//created cloud framework with p5js
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(0);
+
+
+ //Cloud
+fill(255);
+noStroke();
+ellipse(130, 100, 50, 50);
+
+fill(255);
+noStroke();
+ellipse(100, 110, 50, 50);
+
+fill(255);
+noStroke();
+ellipse(160, 110, 50, 50);
+
+fill(255);
+noStroke();
+ellipse(130, 115, 50, 50);
+}
