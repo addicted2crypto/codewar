cd58b3a012042cef76762bd73f85406691f03b85
William <101371762+addicted2crypto@users.noreply.github.com>
2023-07-18 18:10:47 -0700

    daily code challenge for 07/18/2023
    daily code challenge on 07/18/2023

---
+++ codewars07182023.js
@@ -0,0 +1,12 @@
+// Description: Parse float;
+
+// Instructions
+// Output
+// Write function parseF which takes an input and returns a number or null if conversion is not possible. The input can be one of many different types so be aware.
+
+
+
+//   My Solution =>
+
+
+parseF = (s) => /\d/.test(s) ? + s : null;
