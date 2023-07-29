b019ffbec71d87bb0e37d77df683a76bdf605320
William <101371762+addicted2crypto@users.noreply.github.com>
2023-07-28 20:52:41 -0700

    daily code challenge for 07/28/2023
    daily code challenge leet code completed on 07/28/2023

---
+++ leetcode07282023.js
@@ -0,0 +1,74 @@
+// Description: 125. Valid Palindrome
+
+// A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+// Given a string s, return true if it is a palindrome, or false otherwise.
+
+ 
+
+// Example 1:
+
+// Input: s = "A man, a plan, a canal: Panama"
+// Output: true
+// Explanation: "amanaplanacanalpanama" is a palindrome.
+// Example 2:
+
+// Input: s = "race a car"
+// Output: false
+// Explanation: "raceacar" is not a palindrome.
+// Example 3:
+
+// Input: s = " "
+// Output: true
+// Explanation: s is an empty string "" after removing non-alphanumeric characters.
+// Since an empty string reads the same forward and backward, it is a palindrome.
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 2 * 105
+// s consists only of printable ASCII characters.
+
+//   My Solution that took way too long =>
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    let cleanStr = cleanUp(s);
+
+   return isPal(cleanStr);
+
+
+
+};
+
+
+function cleanUp(str) {
+    let char = "abcdefghijklmnopqrstuvwxyz0123456789";
+     let newS= "";
+
+     for(let i = 0; i < str.length; i++){
+         let lCase = str[i].toLowerCase();
+
+         if(char.indexOf(lCase) !== -1){
+             newS += lCase;
+         }
+     }
+            return newS;
+}
+
+        function isPal(str){
+            left = 0;
+            right = str.length -1;
+            while(left < right){
+                if(str[left] !== str[right]){
+                    return false;
+                }
+                left++;
+                right--;
+            }
+            return true;
+        }
+  
