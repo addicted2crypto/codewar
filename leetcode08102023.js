efb5cc7237328b020eddab05f39e6641648aa7b6
William <101371762+addicted2crypto@users.noreply.github.com>
2023-08-10 20:49:20 -0700

    daily code challenge leet code completed on 08/10/2023
    daily code challenge 08/10/2023

---
+++ leetcode08102023.js
@@ -0,0 +1,44 @@
+// Description: Move Zeroes
+
+// Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+// Note that you must do this in-place without making a copy of the array.
+
+ 
+
+// Example 1:
+
+// Input: nums = [0,1,0,3,12]
+// Output: [1,3,12,0,0]
+// Example 2:
+
+// Input: nums = [0]
+// Output: [0]
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 104
+// -231 <= nums[i] <= 231 - 1
+ 
+
+// Follow up: Could you minimize the total number of operations done?
+
+//   My Solution =>
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    l  = 0;
+    r = 0;
+    while(r < nums.length){
+        if(nums[r] !== 0){
+            [nums[l], nums[r]] = [nums[r], nums[l]];
+             l++;
+        }
+         r++;
+    }
+};
+  
